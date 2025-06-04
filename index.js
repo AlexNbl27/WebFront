5e4ee394f0f2e59e3c60ffd438a77fcb0c780780
Alexandre Noblet <alexandrenoblet27@gmail.com>
2025-06-04 15:23:01 +0200

    [feat] implement user searching

--- index.js
+++ index.js
@@ -12,17 +12,36 @@ function renderUsers(users) {
   tbody.innerHTML = "";
   users.forEach((user) => {
     const tr = document.createElement("tr");
+    const gender = user.gender === "male" ? "👨" : "👩";
     tr.innerHTML = `
         <td><img src="${user.picture.thumbnail}" alt="photo"></td>
         <td>${user.name.first} ${user.name.last}</td>
+        <td>${gender}</td>
         <td>${user.email}</td>
         <td>${user.phone}</td>
+        <td>${user.dob.age}</td>
     `;
     tbody.appendChild(tr);
   });
+}
+
+function searchUsersByName(users, searchTerm) {
+  return users.filter((user) =>
+    `${user.name.first} ${user.name.last}`
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase())
+  );
 }
 
 document.getElementById("fetch-users").addEventListener("click", async () => {
   users = await fetchUsers();
   renderUsers(users);
 });
+
+document
+  .getElementById("search-users")
+  .addEventListener("input", async (event) => {
+    const searchTerm = event.target.value;
+    const filteredUsers = searchUsersByName(users, searchTerm);
+    renderUsers(filteredUsers);
+  });
